09059b4e3d1adadd8ed28b3b3f21df00fb2c966a
613668 <beres.613668@swl.k12.oh.us>
2023-10-09 09:41:30 -0400

    dhr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        double tempC;
+        boolean done = false;
+
+        //User enters a temp in celsius
+        do {
+            System.out.println("Enter a temperature in celsius: ");
+            if (scan.hasNextDouble()) {
+                tempC = scan.nextDouble();
+
+            } else { //if temp is not a double
+                System.out.println("Invalid Temp");
+            }
+        } while (!done); //ends loop
     }
 }
