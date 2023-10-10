87ff167ba7b52a405d964a14c16f50761aa2a162
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-10-09 21:01:46 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,32 @@ public class Main {
         //variables
         double tempC;
         boolean done = false;
+        String continueYN;
+        double tempF;
 
-        //User enters a temp in celsius
+        //Loop for user entering temps
         do {
-            System.out.println("Enter a temperature in celsius: ");
-            if (scan.hasNextDouble()) {
-                tempC = scan.nextDouble();
+            System.out.println("Would you like to continue? Enter [Y/N] to stop: "); //prompt for ending the loop
+            continueYN = scan.nextLine();
+            if (continueYN.equals("N")) { //Answering N ends the loop
+                done = true;
+            } else if (continueYN.equals("Y")) { //Asks user for temp if YN prompt = Y
+                System.out.println("Enter a temperature in celsius: ");
+                if (scan.hasNextDouble()) { //if temp is valid, proceed with calculating and outputting tempF
+                    tempC = scan.nextDouble();
+                    tempF = (tempC*9)/5 + 32;
+                    System.out.printf("%-11s %.2f", "Degrees C:", tempC);
+                    System.out.printf("\n%-11s %.2f", "Degrees F:", tempF);
+                    scan.nextLine();
+                    System.out.println("");
 
-            } else { //if temp is not a double
-                System.out.println("Invalid Temp");
+                } else { //if temp is not a double
+                    System.out.println("Invalid Temp");
+                    scan.nextLine();
+                }
+            } else { //invalid response to YN, neither Y nor N
+                System.out.println("Invalid response");
             }
         } while (!done); //ends loop
     }
-}
+} 
